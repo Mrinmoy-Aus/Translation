11e24df4deb29d37dfb1f48cf686cef75eb68397
argho28 <ausmrinmoy2@gmail.com>
2020-11-26 01:07:09 +0530

    translation

---
+++ README.md
@@ -0,0 +1,59 @@
+# Document-Translater
+This repository contains the code to convert English Digital Documents(pdf) into Hindi. 
+
+## Requirements
+
+    1. Python 3.7
+    2. pip 19.0.3
+    3. OS : ubuntu18.** , ununtu16.**
+
+
+1. **Installing Dependencies**
+    ```bash
+    pip install -r requirements.txt
+    sudo apt-get install tesseract-ocr
+    sudo apt install libtesseract-dev 
+    sudo apt-get install libleptonica-dev
+    sudo apt-get install -y poppler-utils
+    python -m textblob.download_corpora
+    ```
+    Please feel free to refer to
+    [tesseract installation](https://github.com/tesseract-ocr/tesseract/wiki/Compiling)
+    page for any help in installation
+    .
+2. **Model Download**: 
+
+    Download model from
+    [here](https://github.com/srijan14/Machine-Translation-Models/tree/master/eng-hin/v1)
+    and copy inside the **./model** folder . Update the model path in src/constants.py file, 
+    if having some different model name.
+    
+3. **Start Server** (will start a server on port 5001)
+    ```bash
+    python src/app.py
+    ```
+4. Go to http://localhost:5001/home
+    
+    ![ http://localhost:5001/home](./static/images/server_demo.png)
+
+
+5. Example Run 
+    
+    a. English - Hindi 
+    
+    Input Upload(English)            |  Hindi Output
+    :-------------------------:|:-------------------------:
+    ![input doc](./static/images/english.png)  |  ![translated doc](./static/images/hindi.png)
+
+**Note: Having too many pages in the pdf might take a bit of time for
+the API to return the results. On successfull processing, a text file
+with the converted hindi text will be generated.**
+
+## Acknowlegements
+1. [text-translator](https://github.com/srijan14/text-translator)
+
+## Future Work
+1. Updating Web UI to allow the user to translate an image format documents, and also the functionality to allow the conversion of a particular page of pdf.
+2. Support for Other Languages.
+
+
---
+++ src/convert.py
@@ -0,0 +1,93 @@
+# Import libraries
+from PIL import Image
+import pytesseract
+import shutil
+import os
+from pdf2image import pdf2image
+from translator import Translator
+from constants import MODEL_PATH
+from textblob import TextBlob
+import uuid
+import re
+
+
+class Convert:
+
+    MODEL_PATH = os.path.abspath(os.path.join(os.getcwd(), MODEL_PATH))
+
+    def __init__(self, filename):
+
+        self.filename = filename
+        self.translator = Translator()
+        self.translator.load_model(self.MODEL_PATH)
+        self.image_out_path = os.path.join(os.getcwd(), "./data/images/")
+        self.outfilename = os.path.join(os.path.dirname(self.filename),
+                                        "output" + str(uuid.uuid1()) + ".txt")
+        self.english = list()
+        self.hindi = list()
+
+    def extract_text(self):
+
+        PDF_file = self.filename
+        out_folder_name = os.path.basename(self.filename)
+
+        if not os.path.exists(self.image_out_path):
+            os.mkdir(self.image_out_path)
+
+        if not os.path.exists(os.path.abspath(os.path.join(self.image_out_path,\
+                out_folder_name))):
+            os.mkdir(os.path.abspath(os.path.join(self.image_out_path,\
+                out_folder_name)))
+
+
+        index=0
+        maxPages = pdf2image._page_count(PDF_file)
+        for page in range(0, maxPages, 10):
+            pages = pdf2image.convert_from_path(PDF_file, dpi=200,
+                                              first_page=page,
+                              last_page=min(page + 10 - 1, maxPages))
+            for tpage in pages:
+                tpage.save(os.path.abspath(os.path.join(self.image_out_path,
+                                                       out_folder_name ,
+                                                       str(index) + ".jpg"
+                                                       )),'JPEG')
+                index = index + 1
+
+        print("Successfully saved images for each page for {}".format(self.image_out_path))
+
+        english_text = list()
+
+        for filename in sorted(os.listdir(os.path.join(self.image_out_path,
+                                                        out_folder_name)),
+                               key=lambda x:int(os.path.splitext(x)[0])):
+            if filename.endswith("jpg"):
+                text = str(((pytesseract.image_to_string(Image.open(os.path.join(self.image_out_path, out_folder_name,filename))))))
+                text = text.replace('-\n', '')
+                english_text.append(text)
+
+        corpus = " ".join(english_text)
+        corpus = re.sub(r'\n+', '\n', corpus).strip()
+        corpus = TextBlob(corpus)
+        for sentence in corpus.sentences:
+            self.english.append(sentence.string.replace("\n"," "))
+        print("English Text Extracted is : {}".format(self.english))
+        shutil.rmtree(self.image_out_path)
+
+
+    def convert_text(self):
+
+        for sourceText in self.english:
+            output = self.translator.translate(sourceText,verbose=False)[0]
+            self.hindi.append(output)
+
+        print("Hindi Converted Text : {}".format(
+            self.hindi))
+
+    def create_pdf(self):
+
+        f = open(self.outfilename, 'w')
+        for item in self.hindi:
+            f.write("%s\n" % item)
+        f.close()
+
+        return self.outfilename
